f64378f22a12ea9a849f95c4b308adeeaa0f17b5
BDGSA <bdgsa@MacBook-Pro-de-BDGSA.local>
2020-09-07 10:53:46 -0600

    prueba de configuracion

--- www/plugin.js
+++ www/plugin.js
@@ -1,37 +1,12 @@
 
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'SmartIDPG';
+var PLUGIN_NAME = 'MiPlugin';
 
-var SmartIDPG = {
+var MiPlugin = {
   saludo: function (name, successCallback, errorCallback){
         exec(successCallback, errorCallback, PLUGIN_NAME, "saludar", [name]);
-  },
-
-  start: function(license, username, strProd, successCallback, errorCallback)
-  { 
-    exec(successCallback, errorCallback, PLUGIN_NAME, "start", [license,username,strProd]);
-
-  },
-
-  unLink: function(channel, session,  successCallback, errorCallback)
-  { 
-    exec(successCallback, errorCallback, PLUGIN_NAME, "start", [channel, session]);
-
-  },
-
-  link: function(channel, session, successCallback, errorCallback)
-  { 
-    exec(successCallback, errorCallback, PLUGIN_NAME, "start", [channel, session]);
-
-  },
-
-  tracking: function(channel, session, activity, successCallback, errorCallback)
-  { 
-    exec(successCallback, errorCallback, PLUGIN_NAME, "start", [channel,session,activity]);
-
   }
-
 };
 
-module.exports = SmartIDPG;
+module.exports = MiPlugin;
